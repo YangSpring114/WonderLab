61d5690841689da354f4a577842609fbfa62f667
hei-shou <blessing_114514@qq.com>
2023-01-12 23:44:04 +0800

    add new animation

--- Views/UsersView.axaml.cs
+++ Views/UsersView.axaml.cs
@@ -6,9 +6,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WonderLab.Modules.Controls;
+using WonderLab.Modules.Media;
 using WonderLab.Modules.Models;
 using WonderLab.Modules.Toolkits;
 using WonderLab.ViewModels;
+using Button = Avalonia.Controls.Button;
 using ComboBoxItem = Avalonia.Controls.ComboBoxItem;
 
 namespace WonderLab.Views
@@ -47,6 +49,40 @@ namespace WonderLab.Views
         private void CancelButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e) => AuthenticatorTypeDialog.Hide();
 
         private void CancelButtonClick1(object? sender, Avalonia.Interactivity.RoutedEventArgs e) => LoginDialog.Hide();
+
+        private void DeleteButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var user = (UserModels)(sender as Button).DataContext!;
+
+            for (int i = 0; i < App.Data.UserList.Count; i++)
+            {
+                if (ViewModel.Users[i].Name == App.Data.UserList[i].UserName && ViewModel.Users[i].Uuid == App.Data.UserList[i].UserUuid)
+                    App.Data.UserList.Remove(App.Data.UserList[i]);
+            }
+
+            ViewModel.Users.Remove(user.Current);
+            var temp = ViewModel.Users;
+            ViewModel.Users = null;
+            ViewModel.Users = new(temp);
+            if (App.Data.SelectedUser is not null && App.Data.SelectedUser.UserName.Equals(user.Name) && App.Data.SelectedUser.UserUuid.Equals(user.Uuid))
+                App.Data.SelectedUser = null;
+
+            MainWindow.ShowInfoBarAsync("成功:",$"账户 {user.Name} 已成功被移除！", InfoBarSeverity.Success);
+        }
+
+        private void BorderImagePointerEnter(object? sender, Avalonia.Input.PointerEventArgs e)
+        {
+            LogToolkit.WriteLine("鼠标移入头像 Border！");
+            ZoomOutAnimation animation = new();
+            animation.RunAsync((Border)sender);
+        }
+
+        private void BorderImagePointerLeave(object? sender, Avalonia.Input.PointerEventArgs e)
+        {
+            LogToolkit.WriteLine("鼠标移出头像 Border！");
+            ZoomOutAnimation animation = new(true);
+            animation.RunAsync((Border)sender);
+        }
     }
 
     partial class UsersView
